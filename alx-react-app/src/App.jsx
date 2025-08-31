80ab08cd11ce829f34cf0f449ce76253c2c0d9d5
UzonduA <129184284+UzonduA@users.noreply.github.com>
2025-08-31 16:39:07 +0100

    fixed errors with component export

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,7 +1,6 @@
-
-import MainContent from './components/MainContent.jsx'
 import WelcomeMessage from './components/WelcomeMessage.jsx'
 import Header from './components/Header.jsx'
+import MainContent from './components/MainContent.jsx'
 import Footer from './components/Footer.jsx'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
