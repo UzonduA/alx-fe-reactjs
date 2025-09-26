0c33895a9b7776b96c44b0243d3be9323f292ec1
UzonduA <129184284+UzonduA@users.noreply.github.com>
2025-09-26 14:37:21 +0100

    Add hover and transition effects

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,13 +1,13 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 sm:p-4 md:p-8 md:max-w-sm mx-auto my-20 rounded-lg shadow-lg max-w-xs">
-        <div className="md:w-36 md:h-36 mx-auto rounded-full overflow-hidden sm:w-24 sm:h-24">
+    <div className="user-profile bg-gray-100 sm:p-4 md:p-8 md:max-w-sm mx-auto my-20 rounded-lg shadow-lg max-w-xs hover:shadow-xl">
+        <div className="md:w-36 md:h-36 mx-auto rounded-full overflow-hidden sm:w-24 sm:h-24 hover:scale-110 transition-transform duration-300 ease-in-out">
             <img 
                 src="https://via.placeholder.com/150"
                 alt="User"
                 className="w-full h-full object-cover"/>
         </div>
-        <h1 className="sm:text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
+        <h1 className="sm:text-lg md:text-xl text-blue-800 my-4 hover:text-blue-500">John Doe</h1>
         <p className="text-gray-600 md:text-base sm:text-sm">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
 
