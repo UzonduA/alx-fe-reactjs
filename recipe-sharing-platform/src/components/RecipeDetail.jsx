97a36f414ab07843e7cbceae9b7605726037d58b
UzonduA <129184284+UzonduA@users.noreply.github.com>
2025-10-02 16:42:05 +0100

    fix: add shadow styling to RecipeDetail component

--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -22,7 +22,7 @@ const RecipeDetail = () => {
   }
 
   return (
-    <div className="max-w-4xl mx-auto p-4">
+    <div className="max-w-4xl mx-auto p-4 shadow-lg rounded-lg">
       <img
         src={recipe.image}
         alt={recipe.title}
